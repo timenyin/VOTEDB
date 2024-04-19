71b0d22837a88d18436e2d0b10e56cc350ac453d
Harmony <47876447+timenyin@users.noreply.github.com>
2024-04-19 14:15:04 +0100

    updating the bootstrap5 pluging

--- createNewPoll.php
+++ createNewPoll.php
@@ -56,6 +56,10 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
     <!-- Favicon -->
     <link rel="shortcut icon" href="assets/favicon/favicon.ico" type="image/x-icon">
 
+    <!-- bs 5 -->
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet"
+        integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
+
     <!-- Map CSS -->
     <link rel="stylesheet" href="../api.mapbox.com/mapbox-gl-js/v0.53.0/mapbox-gl.css">
 
@@ -214,6 +218,10 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
     <?php include('includes/footer.php') ?>
 
     <!-- JAVASCRIPT -->
+    <!-- bs5 JS -->
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js"
+        integrity="sha384-MrcW6ZMFYlzcLA8Nl+NtUVF0sA7MsXsP1UyJoMp4YLEuNSfAP+JcXn/tWtIaxVXM" crossorigin="anonymous">
+    </script>
     <!-- Vendor JS -->
     <script src="assets/js/vendor.bundle.js"></script>
 
--- index.php
+++ index.php
@@ -153,7 +153,7 @@
     <!-- JAVASCRIPT -->
     <!-- Vendor JS -->
 
-    <!-- Map JS -->
+    <!-- bs5 JS -->
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-MrcW6ZMFYlzcLA8Nl+NtUVF0sA7MsXsP1UyJoMp4YLEuNSfAP+JcXn/tWtIaxVXM" crossorigin="anonymous">
     </script>
     <script src="assets/js/vendor.bundle.js"></script>
